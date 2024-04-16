61fcddcce4b6bf53e959fc9953412c889cdf8c4f
KFH <KFH@LAPTOP-TRN-149>
2024-04-16 19:56:35 +0300

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using BankCMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankCMS.Controllers
+{
+    public class CustomerController : Controller
+
+    {
+        
+        Customer[] customersData = [new Customer("32871", "yousef", "yafof@gmail.com", "98322432"), 
+            new Customer("33271", "fawaz", "ads@gmail.com", "9832343432")];
+        
+
+        public IActionResult Index()
+        {
+            return View(customersData);
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace BankCMS.Models
+{
+
+   
+    public class Customer
+    {
+        public string CivilId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+
+
+        public Customer(string CivilId, string Name, string Email, string Phone)
+        {
+            this.CivilId = CivilId;
+            this.Name = Name;   
+            this.Email = Email; 
+            this.Phone = Phone;
+
+        }
+    }
+
+    
+  
+}
